8d29726ce5487b2523166e72ad09a5f79e7bc89d
B. P. Hon <benji.h.peng@gmail.com>
2024-07-27 18:28:23 -0400

    add card

---
+++ src/components/Cards/General.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function General() {
+  return <div>Hello</div>;
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,35 @@
 import { BellIcon, icons } from "lucide-react";
 import { CommandDemo } from "./Command";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Header() {
   return (
     <div className="grid grid-cols-2 gap-4 p-4 border-b">
       <CommandDemo />
       <div className="flex items-center justify-end">
-        <Button variant={"outline"} size={"icon"}>
-          <BellIcon />
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant={"outline"} size={"icon"}>
+              <BellIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );

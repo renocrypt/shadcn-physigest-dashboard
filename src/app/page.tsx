a0a8430f6d8ef02ee62346d79c59f13943941e20
B. P. Hon <benji.h.peng@gmail.com>
2024-07-27 18:49:38 -0400

    add card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import General from "@/components/Cards/General";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Home Page
-      <Button>hello</Button>
+      <General />
     </div>
   );
 }
